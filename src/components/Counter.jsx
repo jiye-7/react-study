8f24161727f8a11d66465105411e6d610fc7c0fe
jiye-7 <jy522hi@gmail.com>
2023-02-19 19:52:37 +0900

    feat: state를 업데이트 하는 방법
    - 이전 state 값을 가지고 업데이트 하는 방법
    - snapshot된 외부 값을 가지고 업데이트하는 방법

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,14 +3,29 @@ import React, { useState } from 'react';
 export default function Counter() {
   const [count, setCount] = useState(0);
 
-  const handleClick = () => {
-    setCount(count + 1);
-  };
-
   return (
     <div className='counter'>
       <span className='number'>{count}</span>
-      <button className='button' onClick={handleClick}>
+      <button
+        className='button'
+        onClick={() => {
+          setCount((prev) => prev + 1);
+          /* 
+          closer, snapshot된 외부 값을 가지고 계산
+          setCount(count + 1);
+          setCount(count + 1);
+          setCount(count + 1);
+          setCount(count + 1);
+          setCount(count + 1); */
+          /* 
+          이전 상태값을 토대로 계산
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1);
+          setCount((prev) => prev + 1); */
+        }}
+      >
         Add +
       </button>
     </div>
