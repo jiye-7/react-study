e28531052df89e81b25fc3042804de3f514ddc7e
jiye-7 <jy522hi@gmail.com>
2023-02-20 00:25:28 +0900

    feat: 고유한 key 값 설정

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,10 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-/**
- * component가 처음 mount되었을 때(처음 보였을 때) 처리해야 되는 일이 있다면 component안에서 했을 경우 무한 루프에 빠지니까 useEffect 안에서 해주면 된다.
- * 딱 1번만 처리해야 될 경우 디펜던시 배열을 비워주면 된다.
- */
-
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
