ec615afb2fd9cfa3f0d275dc3bd9b88b9df2972a
jiye-7 <jy522hi@gmail.com>
2023-02-20 22:45:24 +0900

    feat: 리액트에서 상태를 객체단위로 보관하고 업데이트 하는 방법

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,26 +1,24 @@
 import React, { useState } from 'react';
 import './AppXY.css';
 
+// 리액트에서 상태를 객체단위로 보관하고 업데이트 하는 방법
 export default function AppXY() {
-  /**
-   * PointerEvent 객체에서 clientX, clientY 값 가져오기 -> event가 일어날 때 마다 state에 저장
-   * pointer div에 style={{width: {}, height: {}}} 주기
-   */
-
-  const [clientX, setClientX] = useState(0);
-  const [clientY, setClientY] = useState(0);
+  // 서로 연관있는 data라면 객체로 묶어서 관리(업데이트가 동시에 일어나야 되는 경우)
+  const [position, setPosition] = useState({ clientX: 0, clientY: 0 });
 
   return (
     <div
       className='container'
       onPointerMove={(e) => {
-        setClientX(e.clientX);
-        setClientY(e.clientY);
+        // 만약 수평으로만 이동이 가능하다면?
+        setPosition((prev) => ({ ...prev, clientX: e.clientX }));
       }}
     >
       <div
         className='pointer'
-        style={{ transform: `translate(${clientX}px, ${clientY}px)` }}
+        style={{
+          transform: `translate(${position.clientX}px, ${position.clientY}px)`,
+        }}
       ></div>
     </div>
   );
